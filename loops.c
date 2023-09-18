7900d063911565c0d496eb5ebd6fc47e9993f0f7
parvashukla <76801236+parvashukla@users.noreply.github.com>
2023-09-18 07:55:23 +0530

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,32 @@
+//for loop//
+/*#include<stdio.h>
+int main(){
+    int i;
+    for(i=0; i<=4; i++){
+        printf("My name is Parva \n");
+    }
+    return 0;
+}*/
+
+//while loop//
+/*include<stdio.h>
+int main(){
+    int i = 0;
+    while(i<=10){
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}*/
+
+//do while loop//
+/*#include<stdio.h>
+int main(){
+    int i = 0;
+    do{
+        printf("%d\n", i);
+        i++;
+    }
+    while(i<=10);
+    return 0;
+}*/
---
+++ sum_of_n_numbers.c
@@ -0,0 +1,13 @@
+/*#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number : \n");
+    scanf("%d", &n);
+      int sum = 0;
+    for(int i = 1; i<=n; i++){
+            sum = sum + i;
+
+    }
+    printf("sum is %d\n", sum);
+    return 0;
+}*/
