48bb8721deff184cfd2fbee4550bd1b12c67cd16
parvashukla <76801236+parvashukla@users.noreply.github.com>
2023-09-18 08:16:33 +0530

    Update odd_number_practice_question.c

--- odd_number_practice_question.c
+++ odd_number_practice_question.c
@@ -1,5 +1,5 @@
 //print all the odd numbers between 5 to 50.
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
     for(int i=5; i<=50; i++){
         if(i%2==0){
@@ -8,4 +8,4 @@ int main(){
         printf("%d\n", i);
     }
     return 0;
-}
+}*/
