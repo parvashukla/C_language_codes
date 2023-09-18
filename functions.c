f834a35692d893fba0036434d47cfad545302117
parvashukla <76801236+parvashukla@users.noreply.github.com>
2023-09-18 08:40:05 +0530

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,14 @@
+/*#include<stdio.h>
+    void printHello();
+    void printBye();
+   int main(){
+   printHello();
+   printBye();
+   return 0;
+   }
+   void printHello(){
+   printf("Hello\n");
+   }
+   void printBye(){
+   printf("Bye");
+   }*/
