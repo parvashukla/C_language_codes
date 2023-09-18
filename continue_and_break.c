1b1cf67b22f8c234e2c926854b09b8cb551b5366
parvashukla <76801236+parvashukla@users.noreply.github.com>
2023-09-18 08:08:29 +0530

    Add files via upload

---
+++ continue_and_break.c
@@ -0,0 +1,23 @@
+//break: print all the numbers from 1 to 20 and stop when it come to 13
+/*#include<stdio.h>
+int main(){
+    for(int i = 0; i<=20; i++){
+        if(i == 13){
+            break;
+        }
+        printf("%d\n", i);
+    }
+    return 0;
+}*/
+
+//continue: print all the numbers from 1 to 20 except 8
+/*#include<stdio.h>
+int main(){
+    for(int i = 0; i<=20; i++){
+        if(i == 8){
+            continue;
+        }
+        printf("%d\n", i);
+    }
+    return 0;
+}*/
---
+++ table_of_number.c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(int i = 0; i<=10; i++){
+    printf("%d\n", n*i);
+    }
+
+    return 0;
+}*/
